f16330c56e014e38c80d5b1011c2e8048489f00c
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-04-21 20:00:41 +0000

    first commit

--- app/controllers/multiplication_controllerrr.rb
+++ app/controllers/multiplication_controllerrr.rb
@@ -1,4 +1,4 @@
-class MultiplicationController > application_controller
+class MultiplicationController < ApplicationController
   def show_multiplication_form
     render({ :template => "multiply_template/multiplication_form" })
   end
